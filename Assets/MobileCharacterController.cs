9d0d70b77778ff521bacaa5df4b0628f2972ada1
Toni Laine <fisuworks@gmail.com>
2018-01-13 14:08:35 +0200

    Add empty-ish MobileCharacterController script

---
+++ Assets/MobileCharacterController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileCharacterController : MonoBehaviour {
+	
+	private float lastX;
+	private float lastZ;
+
+	void Start() {
+		lastX = transform.rotation.eulerAngles.x;
+		lastZ = transform.rotation.eulerAngles.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
