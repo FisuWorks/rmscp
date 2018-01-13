aa8b5cb01833bfbf2f5c00cba3cc4bd586794b3d
Toni Laine <fisuworks@gmail.com>
2018-01-13 16:26:23 +0200

    Allow mobile device compass value to determine player rotation

--- Assets/MobileCharacterController.cs
+++ Assets/MobileCharacterController.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MobileCharacterController : MonoBehaviour {
 	
 	private float lastX;
 	private float lastZ;
+	private Vector3 position;
 
 	void Start() {
 		lastX = transform.rotation.eulerAngles.x;
 		lastZ = transform.rotation.eulerAngles.z;
+		position = transform.position;
+		Input.compass.enabled = true;
+		Input.location.Start ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+
+	void Update () {
+		transform.SetPositionAndRotation(position, Quaternion.Euler(0, Input.compass.trueHeading, 0));
 	}
 }
